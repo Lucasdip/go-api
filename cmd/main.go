daa09b5ad7d9b5ed8a0c5e7d322a1f8b72d9cb56
Lucasdip <lucaslimadip@gmail.com>
2026-02-18 19:05:20 -0300

    feat: criação da função para conectar o db

--- cmd/main.go
+++ cmd/main.go
@@ -1,11 +1,32 @@
 package main
+
 import (
+	"go-api/controller"
+	"go-api/db"
+	"go-api/repository"
+	"go-api/usecase"
+
 	"github.com/gin-gonic/gin"
 )
 
 func main() {
 
 server := gin.Default()
+
+dbConnection, err := db.ConnectDB()
+if err != nil {
+	panic(err)
+}
+
+
+// 1. Criamos o repositório e guardamos na variável 'ProductRepository'
+ProductRepository := repository.NewProductRepository(dbConnection)
+
+// 2. Passamos ESSA VARIÁVEL para o UseCase (sem repetir tipos ou nomes extras)
+ProductUsecase := usecase.NewProductUseCase(ProductRepository)
+
+// 3. Passamos o UseCase para o Controller
+productController := controller.NewProductController(ProductUsecase)
 
 server.GET("/ping", func (ctx *gin.Context)  {
 
@@ -15,6 +36,7 @@ server.GET("/ping", func (ctx *gin.Context)  {
 	
 })
 
+server.GET("/products", productController.GetProducts)
 
 server.Run(":8000")
 
--- controller/product_controller.go
+++ controller/product_controller.go
@@ -1,34 +1,32 @@
 package controller
 
 import (
-	"go-api/model"
+	"go-api/usecase"
 	"net/http"
-
 	"github.com/gin-gonic/gin"
 )
 
 type productController struct {
 
-	//
+	ProductUsecase usecase.ProductUsecase
 
 }
 
-func NewProductController() productController {
-	return productController{}
+func NewProductController(usecase usecase.ProductUsecase) productController {
+	return productController{ProductUsecase: usecase}
 }
 
 func (p *productController) GetProducts(ctx *gin.Context) {
 
 
-	products := []model.Product{
-	{
-		ID: 1,
-		Name: "Batata frita",
-		Price: 20,
-
-	},
+	products, err := p.ProductUsecase.GetProducts()
+	if (err != nil) {
+		ctx.JSON(http.StatusInternalServerError, err)
+	}
+	ctx.JSON(http.StatusOK, products)
 }
-ctx.JSON(http.StatusOK, products)
 
-}
+	
+
+
 
